f5cdd9983e5b0a8e692a1e4956ac601c6bbe811d
rlaalst22 <rlaalst22@gmail.com>
2023-07-10 23:19:26 +0900

    init

---
+++ src/test/java/springbook/SpringBookApplicationTests.java
@@ -0,0 +1,13 @@
+package springbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
